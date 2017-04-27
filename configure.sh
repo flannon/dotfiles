3594c6671e8a791c3aacd679255ae3d53ec0db57
Flannon Jackson <flannon@nyu.edu>
2017-04-27 15:38:56 -0400

    Merged submodule updates

--- configure.sh
+++ configure.sh
@@ -2,12 +2,31 @@
 
 set -e
 
-SOURCE=${HOME}/$(dirname $0)
+#SOURCE=${HOME}/$(dirname $0)
 
+case $(dirname $0) in
+  '.')
+    echo "dirname: $(dirname $0)"
+    SOURCE=$(pwd)
+      ;;
+  *)
+    echo "dirname: $(dirname $0)"
+    SOURCE=${HOME}/$(dirname $0)
+      ;;
+esac
 
+# Load submodules
+cd $SOURCE
+git submodule update --init --recursive
+cd -
+
+# Link vim
 if [[ ! -f ${HOME}/.vimrc ]] || [[ ! -d ${HOME}/.vim ]]
 then
   echo "Linking vim"
   ln -s $SOURCE/vimrc $HOME/.vimrc
   ln -s $SOURCE/vim $HOME/.vim
 fi
+
+vim +PluginInstall +qall
+
