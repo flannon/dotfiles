fdd910ff2b3e616cd7cacea78a16fa6e6099e2d9
Flannon Jackson <flannon@nyu.edu>
2018-03-29 08:07:12 -0400

    Added git-packages

--- puppetlabs/code/environments/production/modules/site/profiles/manifests/packages.pp
+++ puppetlabs/code/environments/production/modules/site/profiles/manifests/packages.pp
@@ -69,83 +69,87 @@ class profiles::packages {
   package { 'vagrant': ensure => latest,  provider => 'brewcask', }
     
   # Atom
-  package { 'atom': ensure => latest, provider => 'brewcask', }
+
+  $atom_ensure = latest
+
+  package { 'atom': ensure => $atom_ensure, provider => 'brewcask', }
   
-  package { 'atom-bash-snippets': ensure => latest, provider => apm, }
-  package { 'atom-json': ensure => latest, provider => apm, }
-  package { 'atom-go-jump-test': ensure => latest, provider => apm, }
-  package { 'atom-go-get': ensure => latest, provider => apm, }
-  package { 'atom-gist-dev': ensure => latest, provider     => apm, }
+  package { 'atom-bash-snippets': ensure => $atom_ensure, provider => apm, }
+  package { 'atom-json': ensure => $atom_ensure, provider => apm, }
+  package { 'atom-go-jump-test': ensure => $atom_ensure, provider => apm, }
+  package { 'atom-go-get': ensure => $atom_ensure, provider => apm, }
+  package { 'atom-gist-dev': ensure => $atom_ensure, provider     => apm, }
   package { 'atom-beautify': ensure => absent, provider     => apm, }
-  package { 'atom-runner': ensure => latest, provider       => apm, }
-  package { 'aligner-golang': ensure => latest, provider    => apm, }
-  package { 'autocomplete-json': ensure => latest, provider => apm, }
-  package { 'busy-signal': ensure => latest, provider       => apm, }
-  package { 'color-picker': ensure => latest, provider      => apm, }
-  package { 'declarations': ensure => latest, provider      => apm, }
-  package { 'file-icons': ensure => latest, provider        => apm, }
-  package { 'fonts': ensure => latest, provider   => apm, }
-  package { 'formatter-gofmt': ensure => latest, provider   => apm, }
-  package { 'git-gui': ensure => latest, provider           => apm, }
-  package { 'git-plus': ensure => latest, provider          => apm, }
-  package { 'git-plus-plus': ensure => absent, provider    => apm, }
-  package { 'go-plus': ensure => latest, provider           => apm, }
-  package { 'go-debug': ensure => latest, provider          => apm, }
-  package { 'go-impl': ensure => latest, provider           => apm, }
-  package { 'go-imports': ensure => latest, provider           => apm, }
-  package { 'go-rename': ensure => latest, provider         => apm, }
-  package { 'go-signature-statusbar': ensure => latest, provider  => apm, }
-  package { 'gotests': ensure => latest, provider           => apm, }
-  package { 'go-tip': ensure => latest, provider            => apm, }
-  package { 'go-types': ensure => latest, provider          => apm, }
-  package { 'hclfmt': ensure => latest, provider => apm, }
-  package { 'hyperclick': ensure => latest, provider        => apm, }
-  package { 'atom-ide-ui': ensure => absent, provider        => apm, }
-  package { 'ide-go': ensure => latest, provider            => apm, }
-  package { 'intentions': ensure => latest, provider        => apm, }
-  package { 'intentions-colorpicker': ensure => latest, provider  => apm, }
-  package { 'intentions-numbers': ensure => latest, provider => apm, }
-  package { 'json-converter': ensure => latest, provider     => apm, }
-  package { 'json-to-go-struct': ensure => latest, provider  => apm, }
-  package { 'language-ansible': ensure => latest, provider   => apm, }
-  package { 'last-cursor-position': ensure => latest, provider => apm, }
-  package { 'linter': ensure => latest, provider             => apm, }
-  package { 'linter-ui-default': ensure => latest, provider  => apm, }
-  package { 'linter-ansible-syntax': ensure => latest, provider => apm, }
-  package { 'linter-golinter': ensure => latest, provider       => apm, }
-  package { 'linter-gotype': ensure => latest, provider         => apm, }
-  package { 'linter-packer-validate': ensure => latest, provider => apm, }
-  package { 'linter-vagrant-validate': ensure => latest, provider => apm, }
-  package { 'linter-terraform-syntax': ensure => latest, provider => apm, }
-  package { 'language-puppet': ensure => latest, provider    => apm, }
-  package { 'pretty-json': ensure => present, provider       => apm, }
-  package { 'platformio-ide-terminal': ensure => present, provider => apm, }
-  package { 'script': ensure => present, provider => apm, }
-  package { 'terraform-fmt': ensure => present, provider      => apm, }
-  package { 'term3': ensure => present, provider      => apm, }
-  package { 'you-complete-me': ensure => present, provider    => apm, }
-  package { 'vim-mode-plus': ensure => latest, provider       => apm, }
-  package { 'ex-mode': ensure => latest, provider       => apm, }
+  package { 'atom-runner': ensure => absent, provider       => apm, }
+  package { 'aligner-golang': ensure => $atom_ensure, provider    => apm, }
+  package { 'autocomplete-json': ensure => $atom_ensure, provider => apm, }
+  package { 'busy-signal': ensure => $atom_ensure, provider       => apm, }
+  package { 'color-picker': ensure => $atom_ensure, provider      => apm, }
+  package { 'declarations': ensure => $atom_ensure, provider      => apm, }
+  package { 'file-icons': ensure => $atom_ensure, provider        => apm, }
+  package { 'fonts': ensure => $atom_ensure, provider   => apm, }
+  package { 'formatter-gofmt': ensure => $atom_ensure, provider   => apm, }
+  package { 'git-diff': ensure => $atom_ensure, provider           => apm, }
+  package { 'git-gui': ensure => $atom_ensure, provider           => apm, }
+  package { 'git-plus': ensure => $atom_ensure, provider          => apm, }
+  package { 'git-plus-plus': ensure => $atom_ensure, provider    => apm, }
+  package { 'go-plus': ensure => $atom_ensure, provider           => apm, }
+  package { 'go-debug': ensure => $atom_ensure, provider          => apm, }
+  package { 'go-impl': ensure => $atom_ensure, provider           => apm, }
+  package { 'go-imports': ensure => $atom_ensure, provider           => apm, }
+  package { 'go-rename': ensure => $atom_ensure, provider         => apm, }
+  package { 'go-signature-statusbar': ensure => $atom_ensure, provider  => apm, }
+  package { 'gotests': ensure => $atom_ensure, provider           => apm, }
+  package { 'go-tip': ensure => $atom_ensure, provider            => apm, }
+  package { 'go-types': ensure => $atom_ensure, provider          => apm, }
+  package { 'hclfmt': ensure => $atom_ensure, provider => apm, }
+  package { 'hyperclick': ensure => $atom_ensure, provider        => apm, }
+  package { 'atom-ide-ui': ensure => $atom_ensure, provider        => apm, }
+  package { 'ide-go': ensure => $atom_ensure, provider            => apm, }
+  package { 'intentions': ensure => $atom_ensure, provider        => apm, }
+  package { 'intentions-colorpicker': ensure => $atom_ensure, provider  => apm, }
+  package { 'intentions-numbers': ensure => $atom_ensure, provider => apm, }
+  package { 'json-converter': ensure => $atom_ensure, provider     => apm, }
+  package { 'json-to-go-struct': ensure => $atom_ensure, provider  => apm, }
+  package { 'language-ansible': ensure => $atom_ensure, provider   => apm, }
+  package { 'last-cursor-position': ensure => $atom_ensure, provider => apm, }
+  package { 'linter': ensure => $atom_ensure, provider             => apm, }
+  package { 'linter-ui-default': ensure => $atom_ensure, provider  => apm, }
+  package { 'linter-ansible-syntax': ensure => $atom_ensure, provider => apm, }
+  package { 'linter-golinter': ensure => $atom_ensure, provider       => apm, }
+  package { 'linter-gotype': ensure => $atom_ensure, provider         => apm, }
+  package { 'linter-packer-validate': ensure => $atom_ensure, provider => apm, }
+  package { 'linter-vagrant-validate': ensure => $atom_ensure, provider => apm, }
+  package { 'linter-terraform-syntax': ensure => $atom_ensure, provider => apm, }
+  package { 'language-puppet': ensure => $atom_ensure, provider    => apm, }
+  package { 'pretty-json': ensure => $atom_ensure, provider       => apm, }
+  package { 'platformio-ide-terminal': ensure => $atom_ensure, provider => apm, }
+  package { 'script': ensure => $atom_ensure, provider => apm, }
+  package { 'terraform-fmt': ensure => $atom_ensure, provider      => apm, }
+  package { 'term3': ensure => $atom_ensure, provider      => apm, }
+  package { 'you-complete-me': ensure => $atom_ensure, provider    => apm, }
+  package { 'vim-mode-plus': ensure => $atom_ensure, provider       => apm, }
+  package { 'ex-mode': ensure => $atom_ensure, provider       => apm, }
   
   # Atom Themes and syntax
   package { 'afterglow-monokai-syntax': ensure => absent, provider => apm, }
-  package { 'atom-dark-fusion-syntax': ensure => present, provider => apm, }
-  package { 'base16-syntax': ensure => present, provider => apm, }
+  package { 'atom-dark-fusion-syntax': ensure => absent, provider => apm, }
+  package { 'base16-syntax': ensure => $atom_ensure, provider => apm, }
   package { 'base16-ocean-dark-spacegray': ensure => absent, provider => apm, }
-  package { 'dark-pastel-eighties-syntax': ensure => latest, provider => apm, }
-  package { 'dracula-theme': ensure => latest, provider => apm, }
-  package { 'github-atom-dark-syntax': ensure => present, provider => apm, }
+  package { 'dark-pastel-eighties-syntax': ensure => absent, provider => apm, }
+  package { 'dracula-theme': ensure => absent, provider => apm, }
+  package { 'github-atom-dark-syntax': ensure => absent, provider => apm, }
   package { 'grayscale-syntax': ensure => absent, provider => apm, }
   package { 'immersive-nucleus-dark-ui': ensure => absent, provider => apm, }
-  package { 'leche-syntax': ensure => present, provider => apm, }
+  package { 'leche-syntax': ensure => absent, provider => apm, }
   package { 'monokai-seti': ensure => absent, provider      => apm, }
-  package { 'null-syntax': ensure => present, provider => apm, }
+  package { 'null-syntax': ensure => absent, provider => apm, }
   package { 'one-dark-shade-ui': ensure => absent, provider => apm, }
   package { 'one-dark-matte-syntax': ensure => absent, provider => apm, }
   package { 'nucleus-dark-ui': ensure => absent, provider => apm, }
   package { 'spacegray-dark-neue-ui': ensure => absent, provider => apm, }
   package { 'spacegray-dark-neue-syntax': ensure => absent, provider => apm, }
-  package { 'unite-ui': ensure => present, provider => apm, }
+  package { 'unite-ui': ensure => absent, provider => apm, }
   
 }
 
