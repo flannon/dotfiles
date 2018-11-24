5c3a4f9612e542277e65a7bbb5aec62cc5e057c0
Flannon Jackson <flannon@nyu.edu>
2018-11-24 11:28:56 -0500

    Added minishift config variables

--- puppetlabs/code/environments/production/modules/site/profiles/manifests/bashrc.pp
+++ puppetlabs/code/environments/production/modules/site/profiles/manifests/bashrc.pp
@@ -19,6 +19,27 @@ class profiles::bashrc {
     owner => $identity['user'],
   }
 
+  case $facts['system_profiler']['model_name'] {
+    'MacBook Pro' :  { 
+      alert("model_name:  ${facts['system_profiler']['model_name']}")
+      $minishift_cpus = 2
+      $minishift_memory = 4096
+      $minishift_vm_driver = "virtualbox"
+      $minishift_disk_size = "20GB"
+      alert("cpus, $minishift_cpus, memory, $minishift_memory, vm-driver, $minishift_vm_driver")
+    }
+    'Mac Pro' :  { 
+      alert("case: model name is ${facts['system_profiler']['model_name']}, os.family is ${os['family']}")
+      $minishift_cpus = 8
+      $minishift_memory = 8192
+      $minishift_vm_driver = "virtualbox"
+      $minishift_disk_size = "40GB"
+      alert("cpus, $minishift_cpus, memory, $minishift_memory, vm-driver, $minishift_vm_driver")
+    
+    }
+    default :           { alert("$facts['os']['name'] not supported for minishift at this time") }
+  }
+
   # set up .bashrc
   $bashrc = "/Users/${identity['user']}/.bashrc"
   file { $bashrc :
