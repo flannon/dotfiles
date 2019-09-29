fd551c3ef362ebbb06ce1a4c7e5cddc8e37a223c
Flannon Jackson <flannon@5eight5.com>
2019-09-29 13:30:56 -0400

    Clean up the config scripts a bit

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PATH=$PATH:${HOME}/.local/bin
+INSTALLDIR="${HOME}/.dotfiles"
+
+# Set up authorized_keys for $USER
+grep "${USER}ss\@" ~/.ssh/id_rsa.pub 2>&1 1>/dev/null
+[[ -f ${HOME}/.ssh/id_rsa.pub && $? != 0 ]] && cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
+[[ -f ${HOME}/.ssh/authorized_keys ]] && chmod 644 ${HOME}/.ssh/authorized_keys
+
+stat_container () {
+	container=$1
+	toolbox list --containers | awk 'NR>2 { print $2 }'   \
+	  | grep ${container} 2>&1 1> /dev/null
+}
+
+mktoolbox () {
+  c=$1
+  echo $c
+  stat_container $c
+  [[ $? != 0 ]] && toolbox create --container $c || return 0
+  which ansible 
+  [[ $? != 0 ]] && toolbox run --container $c bash -c "python3 -m pip install ansible --user"
+  toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
+  	${HOME}/.local/bin/ansible-galaxy install -r requirements.yml"
+  wait
+  toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
+  	${HOME}/.local/bin/ansible-playbook -vvv -e user=${USER} -i ./inventory playbooks/$c.yml"
+  wait 
+}
+
+mktoolbox $USER
+mktoolbox control
+
+
--- config.sh
+++ config.sh
@@ -11,8 +11,6 @@ FULL_PATH="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
 SOURCE="$(cd "$(dirname "$0")" && pwd)"
 RED="\033[1;31m" # Red
 NC="\033[0m"     # No Color
-PATH=$PATH:${HOME}/.local/bin
-INSTALLDIR="${HOME}/.dotfiles"
 
 abort() { echo -e "${RED}$* ${NC}" >&2 && exit 1; }
 [[ $USER == "root" ]] && abort \
@@ -38,10 +36,14 @@ if [[ -f .gitmodules ]]; then
 fi
 
 # setup ~/bin
-[[ ! -d $BIN ]] && mkdir $BIN
+if [[ ! -d $BIN ]];
+then
+  mkdir $BIN
+fi
+
+cd ${SOURCE}/bin
 
 # link binaries
-cd ${SOURCE}/bin
 for i in $(ls);
 do
   if [[ ! -L ${HOME}/bin/${i:0:${#i} -3} ]] && \
@@ -53,37 +55,5 @@ do
 done
 cd $OLDPWD
 
-# Set up authorized_keys for $USER
-grep "${USER}ss\@" ~/.ssh/id_rsa.pub 2>&1 1>/dev/null
-[[ -f ${HOME}/.ssh/id_rsa.pub && $? != 0 ]] && cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
-[[ -f ${HOME}/.ssh/authorized_keys ]] && chmod 644 ${HOME}/.ssh/authorized_keys
-
-# Set up controler and $USER containers
-stat_container () {
-	container=$1
-	toolbox list --containers | awk '{ print $2 }' |  \
-	  grep -v ID |  grep -v created | grep ${container} \
-	  2>&1 1> /dev/null
-}
-
-c=$USER
-echo $c
-stat_container $c
-[[ $? != 0 ]] && toolbox create --container $c 
-toolbox run --container $c bash -c "python3 -m pip install ansible --user"
-#toolbox run --container $c bash -c "sudo dnf install -y ansible"
-toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
-	${HOME}/.local/bin/ansible-galaxy install -r requirements.yml"
-toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
-	${HOME}/.local/bin/ansible-playbook -vvvv -e user=${USER} -i ./inventory playbooks/user.yml"
-
-c="control"
-stat_container $c
-[[ $? != 0 ]] && toolbox create --container $c
-toolbox run --container $c bash -c "python3 -m pip install ansible --user"
-#toolbox run --container $c bash -c "sudo dnf install -y ansible python2 python2-pip"
-toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
-	${HOME}/.local/bin/ansible-galaxy install -r requirements.yml"
-toolbox run --container $c bash -c "cd ${INSTALLDIR}/ansible && \
-	${HOME}/.local/bin/ansible-playbook -e user=${USER} -i ./inventory playbooks/control.yml"
-
+# set vim as the git editor
+#git config --global core.editor $(which vim)
