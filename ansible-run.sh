a1456b8faa744db35fdd31804753008929de94e1
Flannon Jackson <flannon@nyu.edu>
2020-03-10 19:36:23 -0400

    Run wihtout

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+HOSTNAME=$(hostname)
+[[ -f /etc/fedora-release ]] && \
+	RELEASE=$(cat /etc/fedora-release | awk '{ print $3 }')
+
 
 usage () {
 cat << EOF
@@ -9,8 +13,8 @@ EOF
 }
 
 echo $1
-[[ -z $1 ]] && usage && exit 1
-[[ $1 != "control" ]] && [[ $1 != $USER ]] && usage && exit 1
+[[ -z $1 ]] && [[ $HOSTNAME == "toolbox" ]] && usage && exit 3
+[[ -n $1 ]] && [[ $1 != "control" ]] && [[ $1 != $USER ]] && usage && exit 2
 #[[ ( $1 != "control" ) || ( $1 != $USER ) ]] && usage && exit 1
 [[ $1 == "control" ]] && PLAYBOOK=$1 
 [[ $1 == $USER ]] && PLAYBOOK="user" 
@@ -22,12 +26,8 @@ abort() { echo -e "${RED}$* ${NC}" >&2 && exit 1; }
 [[ $USER == "root" ]] && abort \
    "Can't be run as root, must be run as $SUDO_USER."
 
-[[ -f /etc/fedora-release ]] && \
-	RELEASE=$(cat /etc/fedora-release | awk '{ print $3 }')
-
 echo $PLAYBOOK
 
-HOSTNAME=$(hostname)
 # configure control
 if [[ $HOSTNAME == "toolbox" ]]; then
   cd ${DIRNAME}/ansible
